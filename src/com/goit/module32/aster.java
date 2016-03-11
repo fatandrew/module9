ccd669422242fb07d0c7643fc7a4d8443f743ad0
Andrew Danylenko <wdanylenko@gmail.com>
2016-03-10 22:58:12 -0800

    Initial commit

---
+++ src/com/goit/module32/aster.java
@@ -0,0 +1,7 @@
+package com.goit.module32;
+
+/**
+ * Created by User on 08.03.2016.
+ */
+public class aster extends flower {
+}
---
+++ src/com/goit/module32/boquetofflowers.java
@@ -0,0 +1,10 @@
+package com.goit.module32;
+
+import java.util.List;
+
+/**
+ * Created by User on 08.03.2016.
+ */
+public class boquetofflowers {
+    List<flower> flowers;
+}
